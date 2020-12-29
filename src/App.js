cf71838f180a0b028e983a3d3dec347f36e13ea5
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2020-12-29 12:24:28 +0200

    Moved MetaTags from App.js clientside to index.html serverside for home page

--- src/App.js
+++ src/App.js
@@ -12,41 +12,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import BlogHome from "./Components/Blog/BlogHome";
 import NavbarBlog from "./Components/NavbarBlog";
 import BlogArticle from "./Components/Blog/BlogArticle";
-import MetaTags from "react-meta-tags";
 
 function App() {
   return (
-    <BrowserRouter basename={window.location.pathname || ""}>
+    <BrowserRouter>
       <div id="page-top">
         <Switch>
           <Route exact path="/">
-            <MetaTags>
-              <meta charSet="utf-8" />
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-              />
-              <meta name="author" content="Radu Alexandru Bulai" />
-              <title>Radu-Alexandru Bulai | Portfolio</title>
-              <meta
-                name="description"
-                content="Radu-Alexandru Bulai Portfolio, Skills and Projects"
-              />
-              <meta
-                property="og:title"
-                content="Radu-Alexandru Bulai | Portfolio"
-              />
-              <meta
-                property="og:description"
-                content="Radu-Alexandru Bulai Portfolio, Skills and Projects"
-              />
-              <meta
-                property="og:image"
-                content="https://lh3.googleusercontent.com/2n6l-SfYcPkDV-ICfAHlpBRMacS-ZD22SVp9fKuGvyjHvk9F9ZR3LrAGs6B7CqjGlj_-vtKlEcJgs9IPZ1umDjq2M8mpDMB1As2ZTX_lchf0GBYxHgvUhAFzvBO2btIPJw_IDzy4v2U=w2400"
-              />
-              <meta property="og:url" content="https://radubulai.com" />
-              <meta name="theme-color" content="#121416" />
-            </MetaTags>
             <div className="container-fluid p-0">
               <Navbar />
               <About />
